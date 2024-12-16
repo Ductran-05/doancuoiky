0c44eb4e6507727b3ab041be4b611f1308d7df8c
Ductran-05 <23520320@gm.uit.edu.vn>
2024-12-17 01:39:19 +0700

    gan cac bang vao tabcontrol

---
+++ doanwpf/doanwpf/AddProduct.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace doanwpf
+{
+    /// <summary>
+    /// Interaction logic for AddProduct.xaml
+    /// </summary>
+    public partial class AddProduct : Window
+    {
+        public AddProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSelectImageClicked(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp"; // Định dạng tệp hỗ trợ
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Tải ảnh và hiển thị trong Image control
+                string filePath = openFileDialog.FileName;
+                BitmapImage bitmap = new BitmapImage(new Uri(filePath));
+                SelectedImage.Source = bitmap;
+            }
+
+        }
+    }
+}
---
+++ doanwpf/doanwpf/HomeWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace doanwpf
+{
+    /// <summary>
+    /// Interaction logic for HomeWindow.xaml
+    /// </summary>
+    public partial class HomeWindow : Window
+    {
+        public HomeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ContenArea.Children.Add(new ProductsControl());
+        }
+        //mở tab
+        #region
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            ContenArea.Children.Add(new WarehouseControl());
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            ContenArea.Children.Add(new ImportInvoiceControl());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ContenArea.Children.Add(new CustomerControl());
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ContenArea.Children.Add(new InvoiceControl());
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            ContenArea.Children.Add(new ProviderControl());
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            ContenArea.Children.Add(new EmployeesControl());    
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            ContenArea.Children.Add(new SalesReportControl());
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            ContenArea.Children.Add(new HomeControl());
+        }
+        #endregion 
+    }
+}
