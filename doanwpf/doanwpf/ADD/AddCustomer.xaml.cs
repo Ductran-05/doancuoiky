a22396e8a32c80dd980113fcf85292c6042c0212
KStuv <lenguyenkhanglop99@gmail.com>
2024-12-28 16:45:09 +0700

    datab

--- doanwpf/doanwpf/ADD/AddCustomer.xaml.cs
+++ doanwpf/doanwpf/ADD/AddCustomer.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using doanwpf.MODEL;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,14 +16,34 @@ using System.Windows.Shapes;
 
 namespace doanwpf
 {
-    /// <summary>
-    /// Interaction logic for AddCustomer.xaml
-    /// </summary>
+    
     public partial class AddCustomer : Window
     {
+        private ObservableCollection<KHACHHANG> _khachhanglist;
+        public ObservableCollection<KHACHHANG> khachhanglist { get => _khachhanglist; set { _khachhanglist = value; } }
         public AddCustomer()
         {
             InitializeComponent();
+
+            DataContext = this;
+        }
+        void loadkhachhangdata()
+        {
+            khachhanglist = new ObservableCollection<KHACHHANG>(dataprovider.Ins.DB.KHACHHANGs.ToList());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string cusName = CustomerName.Text;
+            string gender;
+            if (rbNam.IsChecked == true)
+            {
+                gender = "Nam";
+            }
+            else if (rbNu.IsChecked == true)
+            {
+                gender = "Nữ";
+            }
         }
     }
 }
